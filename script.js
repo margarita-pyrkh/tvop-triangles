b2d33009cb7d76f02021eb9b381c687bdd0d4b39
SQ <ritapyrh07@gmail.com>
2018-03-26 22:58:00 +0300

    Create AlertService

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 
+
 let firstSideInputText;
 let secondSideInputText;
 let thirdSideInputText;
-
-const errorAlert = {
-  title: 'Введены некорректные данные',
-  error: {
-    tooLongValue: 'Данное число не входит в диапазон допустимых значений',
-    notNumber: 'Введено не числовое значение',
-    notInteger: 'Введено не целое число'
-  }
-};
 
 const getInputs = () => {
   firstSideInputText = document.getElementById("firstSideInput").value;
@@ -40,17 +32,7 @@ const filterInteger = (value) =>
 const isSideInteger = (sideValue) => Number.isInteger(sideValue);
 const isValueSafeInteger = (value) => Number.isSafeInteger(value);
 
-const showSweetAlert = () => {
-  swal("Good job!", "You clicked the button!", "success");
-};
 
-const showErrorAlert = (errorMessage) => {
-  swal({
-    type: "error",
-    title: errorAlert.title,
-    text: errorMessage
-  });
-};
 
 const checkTriangle = () => {
   getInputs();
@@ -65,4 +47,4 @@ const checkTriangle = () => {
 $('#triangleInputForm').submit(() => {
   checkTriangle();
   return false;
-})
+});
